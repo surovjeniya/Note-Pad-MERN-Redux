65255db84f93b6fefaf466c1920a2ca99641221c
Evgeniy Surov <surovjeniya@gmail.com>
2022-04-15 17:21:23 +0300

    added client base functonality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
-
-
+import React from 'react'
+import {BrowserRouter as Router} from 'react-router-dom'
+import {AppContext} from './context/AppContext'
+import {useAuth} from './hooks/auth.hook'
+import {useRoutes} from './routes/routes'
 function App() {
+
+  
+  const {token,userId,logOut,logIn} = useAuth()
+  const isLogin = !!token
+  const routes = useRoutes(isLogin)
+
+  const value = {
+    token,userId,logOut,logIn
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <AppContext.Provider value = {value}>
+      <Router>
+        <div className="app">
+          {routes}
+        </div>
+      </Router>
+    </AppContext.Provider>
   );
 }
 
